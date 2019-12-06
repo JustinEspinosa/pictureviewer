29bc3a7a98dd4517abdccdb29a3cf3a0a5f62ae3
Justin Espinosa <justin.espinosa@bluewin.ch>
2019-12-06 14:25:17 +0100

    aktar 7ob

--- src/main/java/com/github/justinespinosa/pictureviewer/ViewPicture.java
+++ src/main/java/com/github/justinespinosa/pictureviewer/ViewPicture.java
@@ -13,24 +13,49 @@ import com.github.justinespinosa.textmode.graphics.core.Resolution;
 import org.apache.commons.cli.*;
 
 import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.font.FontRenderContext;
+import java.awt.font.GlyphVector;
+import java.awt.geom.GeneralPath;
 import java.awt.image.BufferedImage;
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+
+import static java.awt.Font.PLAIN;
+import static java.awt.image.BufferedImage.TYPE_INT_ARGB;
+import static java.awt.image.BufferedImage.TYPE_INT_RGB;
 
 public class ViewPicture {
 
     private static boolean VERBOSE = false;
-    private static Curses CURSES ;
+    private static Curses CURSES;
+    private static Font BASEFONT;
+    static {
+        try {
+            BASEFONT = Font.createFont(Font.TRUETYPE_FONT, ViewPicture.class.
+                    getClassLoader().getResourceAsStream("AdobeArabic-Bold.otf"));
+        }catch (Exception e){
+            throw new RuntimeException(e);
+        }
+    }
 
     private Bitmap bitmap;
+    private int prefix;
     private Resolution resolution;
     private ColorTable colorTable;
 
-    public ViewPicture(BufferedImage image, boolean useShape, double ppc){
+    public ViewPicture(BufferedImage image, boolean useShape, double ppc, int numColors){
         bitmap = new Bitmap(image);
         bitmap.setUseShape(useShape);
-        colorTable = ColorTable.forDepth(ColorDepth.forNumCols(CURSES.numcolors()));
+        colorTable = ColorTable.forDepth(ColorDepth.forNumCols(numColors));
         resolution = new Resolution(ppc);
+    }
+
+    public void setPrefix(int prefix) {
+        this.prefix = prefix;
     }
 
     private static void debug(String s){
@@ -39,30 +64,106 @@ public class ViewPicture {
         }
     }
 
+    private void pad(Curses curses) throws IOException{
+        StringBuilder builder = new StringBuilder();
+        for(int i=0;i<prefix;++i){
+            builder.append(' ');
+        }
+        curses.print(builder.toString());
+    }
 
-    public void display() throws IOException{
+
+    public void display(Curses curses) throws IOException{
 
         debug("Converting picture to ASCII...");
         ASCIIPicture picture = bitmap.ASCIIDither(resolution, colorTable);
         debug("Displaying picture...");
-        CURSES.sc();
+
+        pad(curses);
+        curses.sc();
         int line = 0;
         for(Position p : picture.size()){
 
             if(p.getLine()>line){
                 line = p.getLine();
-                CURSES.rc();
-                CURSES.print("\n");
-                CURSES.sc();
+                curses.rc();
+                curses.print("\n");
+                pad(curses);
+                curses.sc();
             }
 
             ColorChar c = picture.get(p);
-            CURSES.applyColorPair(c.getColors());
-            CURSES.print(String.valueOf(c.getChr()));
+            curses.applyColorPair(c.getColors());
+            curses.print(String.valueOf(c.getChr()));
         }
-        CURSES.rc();
-        CURSES.print("\n");
+        curses.rc();
+        curses.print("\n");
 
+    }
+
+    private static GeneralPath get2alb(){
+        GeneralPath path = new GeneralPath();
+        path.moveTo(100,160);
+        path.lineTo(180,80);
+        path.curveTo(180,20,160,20,140,20);
+        path.curveTo(120,20,100,20,100,60);
+        path.curveTo(100,20,80,20,60,20);
+        path.curveTo(40,20,20,20,20,80);
+        path.lineTo(100,160);
+        return path;
+    }
+
+    private static void withShadow(Graphics2D graphics, Shape shape, Color color, Color shadow){
+        graphics.translate(5,5);
+        graphics.setColor(shadow);
+        graphics.fill(shape);
+        graphics.translate(-5,-5);
+        graphics.setColor(color);
+        graphics.fill(shape);
+    }
+
+    private static Shape getTextShape(Graphics2D graphics, String text){
+        return graphics.getFont()
+                .layoutGlyphVector(graphics.getFontRenderContext(), text.toCharArray(),0,text.length(),Font.LAYOUT_RIGHT_TO_LEFT)
+                .getOutline();
+    }
+
+    public static BufferedImage createImage7ob() throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(
+                        ViewPicture.class.getClassLoader().getResourceAsStream("bo7"),
+                        StandardCharsets.UTF_8));
+        BufferedImage image = new BufferedImage(200,200, TYPE_INT_RGB);
+        Graphics2D imageGraphics = (Graphics2D)image.getGraphics();
+        try {
+            imageGraphics.setStroke(new BasicStroke(3));
+            imageGraphics.setColor(Color.BLUE);
+            imageGraphics.fillRect(0,0,image.getWidth(),image.getHeight());
+            withShadow(imageGraphics, get2alb(), Color.RED, Color.BLACK);
+            imageGraphics.setFont(BASEFONT.deriveFont( Font.BOLD, 60));
+            imageGraphics.translate(8,0);
+
+            int lineHeight = imageGraphics.getFontMetrics().getHeight();
+            String line;
+            while( (line = reader.readLine())!=null){
+                imageGraphics.translate(0,lineHeight);
+                withShadow(imageGraphics,getTextShape(imageGraphics,line),Color.WHITE, Color.BLACK);
+            }
+        }finally {
+            imageGraphics.dispose();
+        }
+        return image;
+    }
+
+
+    private static BufferedImage createImage(CommandLine line) throws IOException {
+        if(line.hasOption("7") && line.hasOption("o") && line.hasOption("b")){
+            return createImage7ob();
+        }
+        String filename = line.getArgList().get(0);
+
+        debug("Loading "+ filename);
+
+        return ImageIO.read(new File(filename));
     }
 
     public static void main(String[] args) throws IOException, ParseException {
@@ -78,7 +179,11 @@ public class ViewPicture {
         Options options = new Options();
         options.addOption( "w", "width", true, "Set image display width in characters" );
         options.addOption( "n", "no-shape", false, "Do not try to guess the shape of zones" );
+        options.addOption( "c", "center", false, "Center the image in the screen" );
         options.addOption( "v", "verbose", false, "Outputs debug information" );
+        options.addOption( "7",  "");
+        options.addOption( "o", "");
+        options.addOption( "b", "");
 
         CommandLine line = parser.parse( options, args );
         if(line.hasOption("v")){
@@ -92,16 +197,13 @@ public class ViewPicture {
 
         debug("CURSES has "+CURSES.numcolors()+" colors.");
 
-        String filename = line.getArgList().get(0);
+        BufferedImage image = createImage(line);
 
-        debug("Loading "+ filename);
-
-        BufferedImage image = ImageIO.read(new File(filename));
-
-        double ppc =  (double)image.getWidth()  /(double)CURSES.cols();
+        int width = CURSES.cols();
         if(line.hasOption("w")){
-            ppc = (double)image.getWidth()  / Double.parseDouble(line.getOptionValue("w"));
+            width = Integer.parseInt(line.getOptionValue("w"));
         }
+        double ppc =  (double)image.getWidth()  /(double)width;
 
         debug("Resolution : "+ppc);
 
@@ -112,10 +214,13 @@ public class ViewPicture {
 
         debug("Use shape : "+useShape);
 
+        ViewPicture vp = new ViewPicture(image, useShape,ppc , CURSES.numcolors());
 
-        ViewPicture vp = new ViewPicture(image, useShape,ppc );
+        if(line.hasOption("c")){
+            vp.setPrefix((int)Math.round(((double)(CURSES.cols() - width))/2.0));
+        }
 
-        vp.display();
+        vp.display(CURSES);
 
     }
 }
---
+++ src/main/java/com/github/justinespinosa/pictureviewer/ViewPictureTcp.java
@@ -0,0 +1,55 @@
+package com.github.justinespinosa.pictureviewer;
+
+import com.github.justinespinosa.textmode.curses.Curses;
+import com.github.justinespinosa.textmode.curses.CursesFactory;
+import com.github.justinespinosa.textmode.curses.net.GeneralSocketIO;
+import com.github.justinespinosa.textmode.curses.net.SocketIO;
+import com.github.justinespinosa.textmode.curses.net.TelnetServer;
+import org.apache.commons.cli.*;
+
+import javax.net.ssl.SSLContext;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.util.concurrent.CompletableFuture;
+
+public class ViewPictureTcp {
+    private final ViewPicture viewPicture;
+    private CursesFactory factory;
+
+    private ViewPictureTcp() throws IOException{
+        factory = CursesFactory.getInstance();
+        viewPicture = new ViewPicture(ViewPicture.createImage7ob(), true, 3, 256);
+    }
+
+    private void handleOne(SocketChannel client){
+        try {
+            Curses curses = factory.createCurses(factory.createTerminal("xterm", new GeneralSocketIO(client)));
+            synchronized (viewPicture) {
+                viewPicture.display(curses);
+            }
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    public void acceptConnections(int port) throws IOException {
+        ServerSocketChannel server = ServerSocketChannel.open();
+        server.socket().bind(new InetSocketAddress(port));
+
+        SocketChannel candidate;
+        while((candidate = server.accept()) != null) {
+            final SocketChannel client = candidate;
+            CompletableFuture.runAsync(() -> handleOne(client));
+        }
+
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        new ViewPictureTcp().acceptConnections(Integer.parseInt(args[0]));
+    }
+
+}
